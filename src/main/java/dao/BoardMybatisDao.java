215c674bc9a28f107299dd545f5fa3b9e70ff481
JiHyun-Son <wlgusdl5736@naver.com>
2023-01-29 02:22:45 +0900

    board base

---
+++ src/main/java/dao/BoardMybatisDao.java
@@ -0,0 +1,92 @@
+package dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import model.Board;
+import mybatis.MybatisConnection;
+
+public class BoardMybatisDao {
+
+	private static final String ns = "board.";
+	private Map map = new HashMap();
+
+	public int insertBoard(Board board) {
+		SqlSession session = MybatisConnection.getConnection();
+		int num = session.insert(ns+"insertBoard", board);
+		MybatisConnection.close(session);
+		
+		return num;
+	}
+	
+	public int boardCount(String boardid) {
+		SqlSession session = MybatisConnection.getConnection();
+		int boardCount = session.selectOne(ns+"boardCount", boardid);
+		MybatisConnection.close(session);
+		
+		return boardCount;
+		
+	}
+	
+	public List<Board> boardList(String boardid, int pageInt, int limit) {
+		/*
+		 * 1 page : 1~3
+		 * 2 page : 4~6
+		 * 3 page : 7~9
+		 * 
+		 * n page : ((page - 1) *limit) + 1 ~ page * limit
+		 * 
+		 * 
+		 */
+		map.clear();
+		map.put("boardid", boardid);
+		map.put("start", ((pageInt - 1) * limit) + 1);
+		map.put("end", (pageInt*limit));
+		
+		SqlSession session = MybatisConnection.getConnection();
+		List<Board> list = session.selectList(ns+"boardList", map);
+		MybatisConnection.close(session);
+		
+		return list;
+		
+	}
+	
+	public Board boardOne(int num) {
+		SqlSession session = MybatisConnection.getConnection();
+		Board board = session.selectOne(ns+"boardOne", num);
+		MybatisConnection.close(session);
+		
+		return board;
+	}
+	
+	public void refStepAdd(int ref, int refstep) {
+		SqlSession session = MybatisConnection.getConnection();
+		map.clear();
+		map.put("ref", ref);
+		map.put("refstep", refstep);
+		
+		session.update(ns+"refStepAdd", map);
+		MybatisConnection.close(session);
+		
+	}
+	
+	public int insertReply(Board board) {
+		SqlSession session = MybatisConnection.getConnection();
+		int num = session.insert(ns+"insertReply", board);
+		MybatisConnection.close(session);
+		return num;
+	}
+	
+	public int boardDelete(int num, String pass) {
+		SqlSession session = MybatisConnection.getConnection();
+		map.clear();
+		map.put("num", num);
+		map.put("pass", pass);
+		int result = session.delete(ns+"deleteBoard", map);
+		MybatisConnection.close(session);
+		return result;
+	}
+}
--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -23,4 +23,127 @@ public class Board {
 	String v_type;
 	String field;
 	String boardid;
+	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getNot_date() {
+		return not_date;
+	}
+	public void setNot_date(Date not_date) {
+		this.not_date = not_date;
+	}
+	public Date getMod_date() {
+		return mod_date;
+	}
+	public void setMod_date(Date mod_date) {
+		this.mod_date = mod_date;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	public String getP_type() {
+		return p_type;
+	}
+	public void setP_type(String p_type) {
+		this.p_type = p_type;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public int getGoal() {
+		return goal;
+	}
+	public void setGoal(int goal) {
+		this.goal = goal;
+	}
+	public int getRaised() {
+		return raised;
+	}
+	public void setRaised(int raised) {
+		this.raised = raised;
+	}
+	public String getReword() {
+		return reword;
+	}
+	public void setReword(String reword) {
+		this.reword = reword;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getAct_time() {
+		return act_time;
+	}
+	public void setAct_time(String act_time) {
+		this.act_time = act_time;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public Date getV_start() {
+		return v_start;
+	}
+	public void setV_start(Date v_start) {
+		this.v_start = v_start;
+	}
+	public Date getV_end() {
+		return v_end;
+	}
+	public void setV_end(Date v_end) {
+		this.v_end = v_end;
+	}
+	public String getV_type() {
+		return v_type;
+	}
+	public void setV_type(String v_type) {
+		this.v_type = v_type;
+	}
+	public String getField() {
+		return field;
+	}
+	public void setField(String field) {
+		this.field = field;
+	}
+	public String getBoardid() {
+		return boardid;
+	}
+	public void setBoardid(String boardid) {
+		this.boardid = boardid;
+	}
+	
+	
 }
